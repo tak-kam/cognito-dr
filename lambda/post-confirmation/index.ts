6c663505c4578e943ed0af313499d08acda5a0b4
Takahiro Kamekura <takahiro.kamekura@mmmcorp.co.jp>
2023-05-05 23:25:56 +0900

    fix: impl lambda for put record

--- lambda/post-confirmation/index.ts
+++ lambda/post-confirmation/index.ts
@@ -1,6 +1,26 @@
 import { PostConfirmationTriggerEvent } from "aws-lambda";
+import { DynamoDB } from "aws-sdk";
+import { DYNAMODB_TABLE_NAME } from "../../const";
 
 export const handler = async (event: PostConfirmationTriggerEvent) => {
-  console.log(event);
-  return event;
+  try {
+    const db = new DynamoDB.DocumentClient();
+    console.log(event);
+    const userName = event.userName;
+    const email = event.request.userAttributes.email;
+    const res = await db
+      .put({
+        TableName: DYNAMODB_TABLE_NAME,
+        Item: {
+          userName,
+          email,
+        },
+      })
+      .promise();
+    console.log(res.$response);
+    return event;
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
 };
--- lib/main-region-stack.ts
+++ lib/main-region-stack.ts
@@ -5,17 +5,74 @@ import { Runtime } from "aws-cdk-lib/aws-lambda";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { UserPool, CfnUserPool } from "aws-cdk-lib/aws-cognito";
 import { BACKUP_REGION, MAIN_REGION } from "../bin/cognito-dr";
-
-export const DYNAMODB_TABLE_NAME = "CognitoBackupTable";
+import { DYNAMODB_TABLE_NAME } from "../const";
 
 export class MainRegionStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // create DynamoDB Global Table
+    const dynamoDbGlobalTable = new CfnGlobalTable(this, "userBackupTable", {
+      tableName: DYNAMODB_TABLE_NAME,
+      attributeDefinitions: [
+        {
+          attributeName: "userName",
+          attributeType: "S",
+        },
+        {
+          attributeName: "email",
+          attributeType: "S",
+        },
+      ],
+      keySchema: [
+        {
+          attributeName: "userName",
+          keyType: "HASH",
+        },
+        {
+          attributeName: "email",
+          keyType: "RANGE",
+        },
+      ],
+      replicas: [
+        {
+          region: MAIN_REGION,
+        },
+        {
+          region: BACKUP_REGION,
+        },
+      ],
+      billingMode: "PAY_PER_REQUEST",
+      streamSpecification: {
+        streamViewType: "NEW_AND_OLD_IMAGES",
+      },
+      sseSpecification: {
+        sseEnabled: true,
+      },
+    });
+
+    const postConfirmationLambdaRole = new cdk.aws_iam.Role(this, "postConfirmationLambdaRole", {
+      assumedBy: new cdk.aws_iam.ServicePrincipal("lambda.amazonaws.com"),
+      managedPolicies: [cdk.aws_iam.ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSLambdaBasicExecutionRole")],
+    });
+
+    const dynamoDbPutItemPolicy = new cdk.aws_iam.Policy(this, "dynamoDbPutItem", {
+      statements: [
+        new cdk.aws_iam.PolicyStatement({
+          actions: ["dynamodb:PutItem"],
+          resources: [dynamoDbGlobalTable.attrArn],
+        }),
+      ],
+    });
+
+    dynamoDbPutItemPolicy.attachToRole(postConfirmationLambdaRole);
+
     const postConfirmationLambda = new NodejsFunction(this, "postConfirmation", {
       entry: "lambda/post-confirmation/index.ts",
       handler: "handler",
       runtime: Runtime.NODEJS_18_X,
+      role: postConfirmationLambdaRole,
+      memorySize: 1024,
     });
 
     // create Cognito userpool
@@ -67,45 +124,5 @@ export class MainRegionStack extends cdk.Stack {
     cfnUserPool.userAttributeUpdateSettings = {
       attributesRequireVerificationBeforeUpdate: ["email"],
     };
-
-    // create DynamoDB Global Table
-    const dynamoDbGlobalTable = new CfnGlobalTable(this, "userBackupTable", {
-      tableName: DYNAMODB_TABLE_NAME,
-      attributeDefinitions: [
-        {
-          attributeName: "userName",
-          attributeType: "S",
-        },
-        {
-          attributeName: "email",
-          attributeType: "S",
-        },
-      ],
-      keySchema: [
-        {
-          attributeName: "userName",
-          keyType: "HASH",
-        },
-        {
-          attributeName: "email",
-          keyType: "RANGE",
-        },
-      ],
-      replicas: [
-        {
-          region: MAIN_REGION,
-        },
-        {
-          region: BACKUP_REGION,
-        },
-      ],
-      billingMode: "PAY_PER_REQUEST",
-      streamSpecification: {
-        streamViewType: "NEW_AND_OLD_IMAGES",
-      },
-      sseSpecification: {
-        sseEnabled: true,
-      },
-    });
   }
 }
