ca19dbf06b8deb131bd99ea4c51627915b304f40
Takahiro Kamekura <takahiro.kamekura@mmmcorp.co.jp>
2023-05-12 12:54:06 +0900

    fix: change backup region

--- const/index.ts
+++ const/index.ts
@@ -1,4 +1,4 @@
 export const DYNAMODB_TABLE_NAME = "CognitoBackupTable";
 
 export const MAIN_REGION = "us-east-1";
-export const BACKUP_REGION = "us-east-2";
+export const BACKUP_REGION = "ap-northeast-1";
