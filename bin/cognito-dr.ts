1d8cfaf0843b5a7817e1e96a51cb4b853b600a39
Takahiro Kamekura <takahiro.kamekura@mmmcorp.co.jp>
2023-05-12 12:54:28 +0900

    add: add backup region stack

--- bin/cognito-dr.ts
+++ bin/cognito-dr.ts
@@ -2,11 +2,18 @@
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { MainRegionStack } from "../lib/main-region-stack";
-import { MAIN_REGION } from "../const";
+import { BACKUP_REGION, MAIN_REGION } from "../const";
+import { BackupRegionStack } from "../lib/backup-region-stack";
 
 const app = new cdk.App();
 new MainRegionStack(app, "MainRegionStack", {
   env: {
     region: MAIN_REGION,
+  },
+});
+
+new BackupRegionStack(app, "BackupRegionStack", {
+  env: {
+    region: BACKUP_REGION,
   },
 });
