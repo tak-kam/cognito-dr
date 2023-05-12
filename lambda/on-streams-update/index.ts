c3dbf963e15caa738ff2b719bd0ebda484078e37
Takahiro Kamekura <takahiro.kamekura@mmmcorp.co.jp>
2023-05-13 06:18:29 +0900

    ffix: add handler for each type of stream events

--- lambda/on-streams-update/index.ts
+++ lambda/on-streams-update/index.ts
@@ -1,10 +1,47 @@
-import { DynamoDBStreamEvent } from "aws-lambda";
+import { DynamoDBRecord, DynamoDBStreamEvent } from "aws-lambda";
 
 export const handler = async (event: DynamoDBStreamEvent) => {
   try {
-    console.log(event);
+    console.log(JSON.stringify(event));
+    event.Records.forEach((record) => {
+      handleRecord(record);
+    });
   } catch (e) {
     console.error(e);
     throw e;
   }
+};
+
+const handleRecord = async (record: DynamoDBRecord) => {
+  switch (record.eventName) {
+    case "INSERT":
+      await onInsert(record);
+      break;
+    case "MODIFY":
+      await onModify(record);
+      break;
+    case "REMOVE":
+      await onRemove(record);
+      break;
+    default:
+      throw new Error(`Unknown event for event: ${record.eventID}`);
+  }
+};
+
+const onInsert = async (record: DynamoDBRecord) => {
+  const userName = record.dynamodb?.NewImage?.userName.S as string;
+  const email = record.dynamodb?.NewImage?.email.S as string;
+  console.log(`Insert: username=${userName}, email=${email}`);
+};
+
+const onModify = async (record: DynamoDBRecord) => {
+  const userName = record.dynamodb?.NewImage?.userName.S as string;
+  const email = record.dynamodb?.NewImage?.email.S as string;
+  console.log(`Modify: username=${userName}, email=${email}`);
+};
+
+const onRemove = async (record: DynamoDBRecord) => {
+  const userName = record.dynamodb?.OldImage?.userName.S as string;
+  const email = record.dynamodb?.OldImage?.email.S as string;
+  console.log(`Remove: username=${userName}, email=${email}`);
 };
