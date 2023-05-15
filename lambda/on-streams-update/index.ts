5ff0a4b4fa40a983785c06c5afbf5c7c1b822fcb
Takahiro Kamekura <takahiro.kamekura@mmmcorp.co.jp>
2023-05-16 07:11:22 +0900

    fix: impl create/update/delete user func

--- lambda/on-streams-update/index.ts
+++ lambda/on-streams-update/index.ts
@@ -1,11 +1,12 @@
 import { DynamoDBRecord, DynamoDBStreamEvent } from "aws-lambda";
+import { CognitoIdentityServiceProvider } from "aws-sdk";
 
 export const handler = async (event: DynamoDBStreamEvent) => {
   try {
     console.log(JSON.stringify(event));
-    event.Records.forEach((record) => {
-      handleRecord(record);
-    });
+    for await (const record of event.Records) {
+      await handleRecord(record);
+    }
   } catch (e) {
     console.error(e);
     throw e;
@@ -32,16 +33,57 @@ const onInsert = async (record: DynamoDBRecord) => {
   const userName = record.dynamodb?.NewImage?.userName.S as string;
   const email = record.dynamodb?.NewImage?.email.S as string;
   console.log(`Insert: username=${userName}, email=${email}`);
+
+  const cognito = new CognitoIdentityServiceProvider();
+  const result = await cognito
+    .adminCreateUser({
+      UserPoolId: process.env.USER_POOL_ID as string,
+      Username: userName,
+      UserAttributes: [
+        {
+          Name: "email_verified",
+          Value: "true",
+        },
+        { Name: "email", Value: email },
+      ],
+    })
+    .promise();
+  console.log("Create user result: ", result);
 };
 
 const onModify = async (record: DynamoDBRecord) => {
   const userName = record.dynamodb?.NewImage?.userName.S as string;
   const email = record.dynamodb?.NewImage?.email.S as string;
   console.log(`Modify: username=${userName}, email=${email}`);
+
+  const cognito = new CognitoIdentityServiceProvider();
+  const result = await cognito
+    .adminUpdateUserAttributes({
+      UserPoolId: process.env.USER_POOL_ID as string,
+      Username: userName,
+      UserAttributes: [
+        {
+          Name: "email_verified",
+          Value: "true",
+        },
+        { Name: "email", Value: email },
+      ],
+    })
+    .promise();
+  console.log("Update user result: ", result);
 };
 
 const onRemove = async (record: DynamoDBRecord) => {
   const userName = record.dynamodb?.OldImage?.userName.S as string;
   const email = record.dynamodb?.OldImage?.email.S as string;
   console.log(`Remove: username=${userName}, email=${email}`);
+
+  const cognito = new CognitoIdentityServiceProvider();
+  const result = await cognito
+    .adminDeleteUser({
+      UserPoolId: process.env.USER_POOL_ID as string,
+      Username: userName,
+    })
+    .promise();
+  console.log("Delete user result: ", result);
 };
